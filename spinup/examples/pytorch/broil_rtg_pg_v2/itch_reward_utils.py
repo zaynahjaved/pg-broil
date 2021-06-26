aa165a57ac0ed05d4e1827f0e3ba4ff984e5a162
zaynahjaved <33410590+zaynahjaved@users.noreply.github.com>
2021-06-25 21:34:54 -0700

    Delete itch_reward_utils.py

--- spinup/examples/pytorch/broil_rtg_pg_v2/itch_reward_utils.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-class ItchReward():
-    def __init__(self):
-    	# corresponds to probabilities of wanting to itch on [shoulder, elbow]
-        self.posterior = np.array([0.7, 0.3])
-
-    def get_reward_distribution(self, env, a):
-        # return reward for each of these
-        return np.array([env.get_reward(a, limb='shoulder'), env.get_reward(a, limb='elbow')])
