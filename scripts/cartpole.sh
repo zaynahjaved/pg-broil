2690e1b3471f173d2e43aa71bfb4cb8cd17bccf2
Zaynah Javed <zjaved@jensen.ist.berkeley.edu>
2021-06-29 13:52:11 -0700

    starting scripts

---
+++ scripts/cartpole.sh
@@ -0,0 +1,5 @@
+python ../spinup/algos/pytorch/vpg/broil_vpg.py --env CartPole-v0 --broil_lambda 1
+python ../spinup/algos/pytorch/vpg/broil_vpg.py --env CartPole-v0 --broil_lambda 0.98
+python ../spinup/algos/pytorch/vpg/broil_vpg.py --env CartPole-v0 --broil_lambda 0.9
+python ../spinup/algos/pytorch/vpg/broil_vpg.py --env CartPole-v0 --broil_lambda 0.75
+python ../spinup/algos/pytorch/vpg/broil_vpg.py --env CartPole-v0 --broil_lambda 0
